05c1a843482480d37d5d0d39d09090c1e1a08ad7
andykwan1124 <woody.andygrant@gmail.com>
2020-06-30 17:50:47 -0700

    added first styled component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,22 @@
 import Head from 'next/head'
+import styled from 'styled-components'
+
+const StyledLogin = styled.div`
+    position: absolute;
+    width: 85px;
+    height: 40px;
+    left: 842px;
+    top: 179px;
+
+    font-family: HK Grotesk;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 40px;
+    text-align: center;
+
+    color: #000000;
+`
 
 export default function Home() {
   return (
--- pages/landing.js
+++ pages/landing.js
@@ -1,5 +1,3 @@
-
-
 export default () => (
     <div>Landing Page</div>
 )

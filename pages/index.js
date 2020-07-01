b5da6477902fdd3a01ec76f5d7ff6c4717dd5a46
andykwan1124 <woody.andygrant@gmail.com>
2020-07-01 16:36:37 -0700

    added login button and some divs

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,9 @@ import { useState } from 'react'
 import { COLOR } from '../constants'
 import { Helmet } from 'react-helmet'
 import { GlobalStyle } from '../components/globalStyles'
+import Button from '../components/button'
 
-const StyledLogIn = styled.div`
+const LogInDiv = styled.div`
     position: absolute;
     width: 85px;
     height: 40px;
@@ -18,6 +19,82 @@ const StyledLogIn = styled.div`
     color: ${ COLOR.BLACK };
 `
 
+const UserNameDiv = styled.div`
+    position: absolute;
+    width: 119px;
+    height: 30px;
+    left: 702px;
+    top: 285px;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 30px;
+    color: ${ COLOR.BLACK };
+    text-align: center;
+`
+
+const PasswordDiv = styled.div`
+    position: absolute;
+    width: 102px;
+    height: 30px;
+    left: 702px;
+    top: 400px;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 30px;
+    text-align: center;
+    color: ${ COLOR.BLACK };
+`
+
+const ForgotPasswordDiv = styled.div`
+    position: absolute;
+    width: 129px;
+    height: 20px;
+    left: 939px;
+    top: 488px;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 20px;
+    text-align: center;
+    color: ${ COLOR.BLACK };
+`
+
+const ButtonWrapper = styled.div`
+    margin-left: 835px;
+    margin-top: 588px;
+`
+
+const Wrapper = styled.div`
+
+`
+
+const UserNameInput = styled.input`
+    position: absolute;
+    width: 365px;
+    height: 40px;
+    left: 702px;
+    top: 326px;
+    background: ${ COLOR.WHITE };
+    border: 1px solid #606060;
+    box-sizing: border-box;
+    border-radius: 2px;
+    font-size: 16px;
+    line-height: 20px;
+`
+
+const PasswordInput = styled.input`
+    position: absolute;
+    width: 365px;
+    height: 40px;
+    left: 702px;
+    top: 441px;
+    background: ${ COLOR.WHITE };
+    border: 1px solid #606060;
+    box-sizing: border-box;
+    border-radius: 2px;
+    font-size: 16px;
+    line-height: 20px;
+`
+
 
 export default function Home() {
   return (
@@ -26,7 +103,16 @@ export default function Home() {
       <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/hk-grotesk" type="text/css"/>
       </Helmet>
       <GlobalStyle/>
-      <StyledLogIn>Log in</StyledLogIn>
+      <LogInDiv>Log iin</LogInDiv>
+      <UserNameDiv>User Name</UserNameDiv>
+      <UserNameInput/>
+      <PasswordDiv>Password</PasswordDiv>
+      <PasswordInput/>
+      <ForgotPasswordDiv>Forgot password?</ForgotPasswordDiv>
+      <ButtonWrapper>
+        <Button>Login</Button>
+      </ButtonWrapper>
+      
     </React.Fragment>
   )
 }

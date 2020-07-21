d7288b76dffc72db8e73fe4005b35b449ecb82d0
Ian Mah <ian.mah@hootsuite.com>
2020-07-21 00:51:08 -0700

    fix subpages

--- pages/[id]/sponsors.js
+++ pages/[id]/sponsors.js
@@ -1,8 +1,47 @@
 import React from 'react'
-import Page from '../../../components/page'
+import Page from '../../components/page'
+import { db } from '../../utility/firebase'
 
-export default ({id}) => (
-  <Page>
+export default ({id, hackathons}) => (
+  <Page currentPath={id} hackathons={hackathons}>
     Sponsors
   </Page>
 ) 
+
+const getHackathonIds = async () => {
+  return db.collection('Hackathons').get().then(querySnapshot => {
+    const paths = []
+    querySnapshot.forEach(doc => {
+      paths.push({
+        params: {
+          id: doc.id
+        }
+      })
+    })
+    return paths
+  })
+}
+
+export const getStaticPaths = async () => {
+  const paths = await getHackathonIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async ({ params }) => {
+
+  return await db.collection('Hackathons').get().then(querySnapshot => {
+    const hackathons = []
+    querySnapshot.forEach(doc => {
+        hackathons.push(doc.id)
+    })
+    return {
+      props: {
+        hackathons,
+        id: params.id
+      }
+    }
+  })
+}

60a9605378634c73bcc46d1d8fa781534038cc4e
andykwan1124 <woody.andygrant@gmail.com>
2020-07-24 04:07:37 -0700

    removed part of env set up in next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,39 @@
+// module.exports = () => {
+//   const envVars = 
+//     process.env.DEPLOY_ENV == 'PRODUCTION' ?  
+//     {
+//       env: {
+//       NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyDGa7alU0NhfBATSQ6CalkY4Za9wWPrM7o",
+//       NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "nwplus-ubc.firebaseapp.com",
+//       NEXT_PUBLIC_FIREBASE_DATABASE_URL: "https://nwplus-ubc.firebaseio.com",
+//       NEXT_PUBLIC_FIREBASE_PROJECT_ID: "nwplus-ubc",
+//       NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "nwplus-ubc.appspot.com",
+//       NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "G-BT2W7DSL1G",
+//       NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "306881258768",
+//       NEXT_PUBLIC_FIREBASE_APP_ID: "1:306881258768:web:bc922148732abee79f7195"
+//       }
+//     } : 
+//     {
+//       env: {
+//       NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//       NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//       NEXT_PUBLIC_FIREBASE_DATABASE_URL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+//       NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//       NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//       NEXT_PUBLIC_FIREBASE_APP_ID:  process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//       NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//       NEXT_PUBLIC_RECAPTCHA_SITE_KEY:  process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+//       }
+//     }
+//   return {
+//     ...envVars
+//   }
+// }
+
 module.exports = () => {
-  const envVars = 
-    process.env.DEPLOY_ENV == 'PRODUCTION' ?  
-    {
-      env: {
+  if (process.env.DEPLOY_ENV !== "PRODUCTION") return {}
+  return {
+    env: {
       NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyDGa7alU0NhfBATSQ6CalkY4Za9wWPrM7o",
       NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "nwplus-ubc.firebaseapp.com",
       NEXT_PUBLIC_FIREBASE_DATABASE_URL: "https://nwplus-ubc.firebaseio.com",
@@ -11,21 +42,6 @@ module.exports = () => {
       NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "G-BT2W7DSL1G",
       NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "306881258768",
       NEXT_PUBLIC_FIREBASE_APP_ID: "1:306881258768:web:bc922148732abee79f7195"
-      }
-    } : 
-    {
-      env: {
-      NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      NEXT_PUBLIC_FIREBASE_DATABASE_URL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-      NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      NEXT_PUBLIC_FIREBASE_APP_ID:  process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-      NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      NEXT_PUBLIC_RECAPTCHA_SITE_KEY:  process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
-      }
     }
-  return {
-    ...envVars
   }
 }

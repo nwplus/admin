5fde3f526888f7c4554b673ec8486309ff27aa8c
JennyP <panjenny0@gmail.com>
2020-08-03 20:34:08 -0400

    updated ci.yml with embedded env secrets

--- utility/firebase.js
+++ utility/firebase.js
@@ -11,7 +11,7 @@ if (!firebase.apps.length) {
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID

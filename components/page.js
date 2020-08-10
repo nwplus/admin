cd2555a312ee070681662d24b471ce69d87830e2
Ian Mah <ian.mah@hootsuite.com>
2020-08-10 15:57:55 -0700

    Auto fix prettier

--- components/page.js
+++ components/page.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import styled from 'styled-components'
-import Sidebar from './sidebar'
+import React from 'react';
+import styled from 'styled-components';
+import Sidebar from './sidebar';
 
 const Container = styled.div`
   display: flex;
   align-items: stretch;
   min-height: 100vh;
-`
+`;
 
 const Content = styled.div`
   padding: 60px;
-`
+`;
 
-export default ({hackathons, currentPath, children}) => (
+export default ({ hackathons, currentPath, children }) => (
   <Container>
-    <Sidebar currentPath={currentPath} hackathons={hackathons}/>
+    <Sidebar currentPath={currentPath} hackathons={hackathons} />
     <Content>{children}</Content>
   </Container>
-) 
+);

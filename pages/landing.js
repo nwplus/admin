3cf81b727e675a60dffb3b2247277ba6b98bf029
Giulio Rossi <ciuffi9@mac.com>
2020-09-09 19:18:18 -0700

    add landing page

--- pages/landing.js
+++ pages/landing.js
@@ -1,31 +1,53 @@
 import React from 'react';
-import firebase from 'firebase';
-import Card, {
-  CardHeader,
-  CardTitle,
-  CardContent,
-  CardButtonContainer,
-} from '../components/card';
-import { EDIT } from '../constants';
-import Button from '../components/button';
+import styled from 'styled-components';
+import Sidebar from '../components/sidebar';
+import { getHackathons } from '../utility/firebase';
+import nwPlusLogo from '../assets/nwplus.svg';
 
-export default function Landing() {
+const Container = styled.div`
+  display: flex;
+  align-items: stretch;
+  min-height: 100vh;
+`;
+
+const NwPlusImage = styled.img`
+  margin-top: 5%;
+  width: 250px;
+  height: 250px;
+  margin-bottom: 2%;
+`;
+
+const WelcomeTitle = styled.h1`
+  font-size: 60px;
+`;
+
+const InfoMessage = styled.p`
+  font-size: 40px;
+`;
+
+export default function Landing({ hackathons, id }) {
   return (
-    <>
-      <div>Login Page</div>
-
-      <Card>
-        <CardHeader>
-          <CardTitle>Hi</CardTitle>
-          <p>Some extra text</p>
-          <CardButtonContainer>
-            <Button type={EDIT}>Hi there</Button>
-
-            <Button onClick={() => firebase.auth().signOut()}>Sign out</Button>
-          </CardButtonContainer>
-        </CardHeader>
-        <CardContent>Example usage of card component</CardContent>
-      </Card>
-    </>
+    <Container>
+      <Sidebar currentPath={id} hackathons={hackathons} />
+      <div style={{ width: '80vw' }}>
+        <div style={{ width: '100%', textAlign: 'center' }}>
+          <NwPlusImage src={nwPlusLogo} />
+          <WelcomeTitle>Welcome to the CMS!</WelcomeTitle>
+          <InfoMessage>
+            {'<'} Please choose a hackathon from the sidebar.
+          </InfoMessage>
+        </div>
+      </div>
+    </Container>
   );
 }
+
+export const getStaticProps = async ({ params }) => {
+  const hackathons = await getHackathons();
+  return {
+    props: {
+      hackathons,
+      id: params?.id ?? null,
+    },
+  };
+};

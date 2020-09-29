ff3d307f75257e5b2cb78f1072b27ab31e81ae4d
Giulio Rossi <ciuffi9@mac.com>
2020-09-28 22:45:54 -0700

    add CMSControl component to _app.js

--- utility/CMSControl.js
+++ utility/CMSControl.js
@@ -1,18 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SubscribeToCMSStatus } from './firebase';
 import CMSUnderConstructionPage from '../components/CMSUnderConstructionPage';
+import LoadingPage from '../components/LoadingPage';
 
 export default function CMSControl({ children }) {
-    const [CMSOff, setCMSOff] = useState(null);
-    const [CMSOffDate, setCMSOffDate] = useState();
-    useEffect(() => {
-        const unsubscribe = SubscribeToCMSStatus(setCMSOff, setCMSOffDate);
-        return unsubscribe;
-    }, []);
+  const [CMSOff, setCMSOff] = useState(null);
+  const [CMSOffDate, setCMSOffDate] = useState();
+  useEffect(() => {
+    const unsubscribe = SubscribeToCMSStatus(setCMSOff, setCMSOffDate);
+    return unsubscribe;
+  }, []);
 
-    return CMSOff ? (
-        <CMSUnderConstructionPage date={CMSOffDate} />
-    ) : (
-            <>{children} </>
-        );
+  return CMSOff === null ? (
+    <LoadingPage />
+  ) : CMSOff ? (
+    <CMSUnderConstructionPage date={CMSOffDate} />
+  ) : (
+    <>{children} </>
+  );
 }

dffe666d3e2bf8c7dc546af7978471724cd26c1a
Ian <ianmmah@gmail.com>
2020-10-09 01:49:00 -0700

    Copy components

--- pages/Livesite/settings.js
+++ pages/Livesite/settings.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import Page from '../../components/page';
-import { getHackathons } from '../../utility/firebase';
-import { LIVESITE_NAVBAR } from '../../constants';
+import Card, {
+  CardHeader,
+  CardButtonContainer,
+  CardTitle,
+  CardContent,
+} from '../../components/card';
+import { ModalField, ModalButton } from '../../components/modal';
+import Button from '../../components/button';
+import { formatDate, getHackathons } from '../../utility/firebase';
+import { LIVESITE_NAVBAR, VIEW, EDIT } from '../../constants';
 
 export default ({ hackathons }) => (
   <Page
@@ -9,13 +17,37 @@ export default ({ hackathons }) => (
     hackathons={hackathons}
     navbarItems={LIVESITE_NAVBAR}
   >
-    <img
-      src="https://i.imgur.com/lEjmB7U.gif"
-      width="480"
-      height="362"
-      alt="panik"
-    />
-    <p>Settings - This area under construction.</p>
+    <Card>
+      <CardHeader>
+        <CardTitle>Livesite Settings</CardTitle>
+        <p>{`Last edited by ${"asd"} at ${formatDate(
+          123
+        )}`}</p>
+        <CardButtonContainer>
+          <Button
+            type={EDIT}
+            onClick={() => {
+              console.log('hey')
+            }}
+          />
+        </CardButtonContainer>
+      </CardHeader>
+      <CardContent>
+        <ModalField
+          label="Question"
+          modalAction={EDIT}
+          onChange={(event) => {
+            console.log("hi")
+          }}
+        >
+          GHuuuuhashd
+        </ModalField>
+        <ModalButton
+          type={EDIT}
+          handleSave={() => console.log('save')}
+        />
+      </CardContent>
+    </Card>
   </Page>
 );
 

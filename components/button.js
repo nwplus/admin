beafe5d1a47dc9c6dd83b9fc52cd82e3ef269102
Derek Chen <derekc150@gmail.com>
2021-11-09 17:55:12 -0800

    implemented charcuterie for buttons

--- components/button.js
+++ components/button.js
@@ -3,32 +3,31 @@ import styled, { css } from 'styled-components';
 import Icon from './Icon';
 import { COLOR, EDIT, VIEW, NEW, CLOSE, DELETE } from '../constants';
 
-const StyledButton = css`
-  border: none;
+const StyledButton = styled.button`
+  ${(props) => props.outlined ? `border: 2px solid ${props.disabled ? COLOR.INACTIVE_DARK_GRAY : props.contentColor}` : `border: none`};
   font-family: 'HK Grotesk';
   font-weight: bold;
   ${(props) => props.isText && 'padding: 6px 24px; height: 40px;'}
   ${(props) =>
     !props.isText && props.contentColor === COLOR.BLACK && 'padding: 10px;'}
   ${(props) =>
-    props.color
-      ? `color: ${props.contentColor}; background: ${props.color};`
-      : `color: ${COLOR.WHITE}; background-color: ${COLOR.PRIMARY};`}
+    !props.disabled ? (
+      props.color
+        ? `color: ${props.contentColor}; background: ${props.color};`
+        : `color: ${COLOR.WHITE}; background-color: ${COLOR.PRIMARY};`
+    ) : (
+      `color: ${COLOR.INACTIVE_DARK_GRAY}; background: ${props.outlined ? COLOR.TRANSPARENT : COLOR.UNSELECTED_GRAY};`
+    )}
+  border-radius: ${(props) => props.rounded ? (props.inline ? '0 100px 100px 0' : '100px') : (props.inline ? '0 8px 8px 0' : '8px')};
   cursor: pointer;
   display: flex;
   align-items: center;
   font-size: 16px;
+  &:hover {
+    color: ${(props) => props.hoverContentColor};
+    background: ${(props) => props.hoverBackgroundColor};
+  }
 `;
-
-const RectangleStyledButton = styled.button`
-  ${StyledButton}
-  border-radius: ${(props) => (props.inline ? '0 3px 3px 0' : '3px')};
-`;
-
-const RoundStyledButton = styled.button`
-  ${StyledButton}
-  border-radius: ${(props) => (props.inline ? '0 100px 100px 0' : '100px')};
-`
 
 const StyledIcon = styled(Icon)`
   ${(props) => props.hasText && 'margin-right: 8px;'}
@@ -46,13 +45,23 @@ const Button = ({
   contentColor = COLOR.BLACK,
   onClick,
   inline = false,
+  hoverBackgroundColor,
+  hoverContentColor,
+  disabled = false,
+  outlined = false,
+  rounded = false
 }) => (
-  <RectangleStyledButton
+  <StyledButton
     isText={children && !type}
     onClick={onClick}
     color={color}
     contentColor={contentColor}
     inline={inline}
+    hoverBackgroundColor={hoverBackgroundColor}
+    hoverContentColor={hoverContentColor}
+    disabled={disabled}
+    outlined={outlined}
+    rounded={rounded}
   >
     {type === EDIT && !color && <StyledIcon hasText={children} icon='edit' />}
     {type === EDIT && color && <Icon color={COLOR.BLACK} icon='edit' />}
@@ -61,32 +70,7 @@ const Button = ({
     {type === DELETE && <Icon icon='times' color={'red'} />}
     {type === CLOSE && <Icon icon='times' color={contentColor} />}
     {children}
-  </RectangleStyledButton>
-);
-
-export const RoundedButton = ({
-  children,
-  type,
-  color,
-  contentColor = COLOR.BLACK,
-  onClick,
-  inline = false,
-}) => (
-  <RoundStyledButton
-    isText={children && !type}
-    onClick={onClick}
-    color={color}
-    contentColor={contentColor}
-    inline={inline}
-  >
-    {type === EDIT && !color && <StyledIcon hasText={children} icon='edit' />}
-    {type === EDIT && color && <Icon color={COLOR.BLACK} icon='edit' />}
-    {type === NEW && <StyledIcon hasText={children} icon='plus' />}
-    {type === VIEW && <Icon icon='search' />}
-    {type === DELETE && <Icon icon='times' color={'red'} />}
-    {type === CLOSE && <Icon icon='times' color={contentColor} />}
-    {children}
-  </RoundStyledButton>
+  </StyledButton>
 );
 
 export default Button;

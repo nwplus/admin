1b03e6d0f95014fb94bcfaf977f89bc0a80a3feb
Derek Chen <derekc150@gmail.com>
2021-11-09 18:10:09 -0800

    remove unnecessary import

--- components/button.js
+++ components/button.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import Icon from './Icon';
 import { COLOR, EDIT, VIEW, NEW, CLOSE, DELETE } from '../constants';
 

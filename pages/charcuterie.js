d1dc68c95a3721293b78758020867b4003cabe17
Derek Chen <45786074+dchen150@users.noreply.github.com>
2021-12-08 14:29:22 -0800

    implement scoring item component (#143)
    * implement scoring item component
    
    * remove comment box, change colors

--- pages/charcuterie.js
+++ pages/charcuterie.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Button from '../components/button';
 import { Title1, Title2, Title3, Title4 } from '../components/Typography';
 import Tag from '../components/tag';
+import ScoreInput from '../components/scoreInput';
 import { COLOR, BUTTON_COLOR } from '../constants';
 
 const Container = styled.div`
@@ -30,211 +31,227 @@ const TagContainer = styled.div`
 
 export default function Charcuterie() {
     return (
-        <Container>
+        <>
+            <Container>
+                <Section>
+                    <Row>
+                        <ButtonContainer>
+                            <Button color={BUTTON_COLOR.PRIMARY} contentColor={COLOR.MIDNIGHT_PURPLE} >
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button color={BUTTON_COLOR.PRIMARY} contentColor={COLOR.MIDNIGHT_PURPLE} hoverContentColor={COLOR.TEAL} hoverBackgroundColor={BUTTON_COLOR.HOVER_PRIMARY} >
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button disabled >
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                    </Row>
+
+                    <Row>
+                        <ButtonContainer>
+                            <Button color={BUTTON_COLOR.SECONDARY} contentColor={COLOR.MIDNIGHT_PURPLE} >
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button color={BUTTON_COLOR.SECONDARY} contentColor={COLOR.MIDNIGHT_PURPLE} hoverBackgroundColor={COLOR.LIGHT_GRAY}>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button disabled >
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                    </Row>
+
+                    <Row>
+                        <ButtonContainer>
+                            <Button color={COLOR.TRANSPARENT} contentColor={COLOR.NW_TEAL} hoverContentColor={COLOR.TEAL} outlined>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button color={COLOR.TRANSPARENT} contentColor={COLOR.NW_TEAL} hoverContentColor={COLOR.TEAL} hoverBackgroundColor={BUTTON_COLOR.HOVER_PRIMARY} outlined>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button disabled outlined>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+                    </Row>
+
+                    <Row>
+                        <ButtonContainer>
+                            <Button color={BUTTON_COLOR.DESTRUCTIVE} contentColor={COLOR.WHITE}>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button color={BUTTON_COLOR.DESTRUCTIVE} contentColor={COLOR.WHITE} hoverBackgroundColor={COLOR.BRIGHT_RED} >
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button disabled>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+                    </Row>
+
+                </Section>
+
+                <Section>
+                    <Row>
+                        <ButtonContainer>
+                            <Button color={BUTTON_COLOR.PRIMARY} contentColor={COLOR.MIDNIGHT_PURPLE} rounded>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button color={BUTTON_COLOR.PRIMARY} contentColor={COLOR.MIDNIGHT_PURPLE} hoverContentColor={COLOR.TEAL} hoverBackgroundColor={BUTTON_COLOR.HOVER_PRIMARY} rounded>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button disabled rounded>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                    </Row>
+
+                    <Row>
+                        <ButtonContainer>
+                            <Button color={BUTTON_COLOR.SECONDARY} contentColor={COLOR.MIDNIGHT_PURPLE} rounded>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button color={BUTTON_COLOR.SECONDARY} contentColor={COLOR.MIDNIGHT_PURPLE} hoverBackgroundColor={COLOR.LIGHT_GRAY} rounded>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button disabled rounded>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                    </Row>
+
+                    <Row>
+                        <ButtonContainer>
+                            <Button color={COLOR.TRANSPARENT} contentColor={COLOR.NW_TEAL} hoverContentColor={COLOR.TEAL} outlined rounded>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button color={COLOR.TRANSPARENT} contentColor={COLOR.NW_TEAL} hoverContentColor={COLOR.TEAL} hoverBackgroundColor={BUTTON_COLOR.HOVER_PRIMARY} outlined rounded>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button disabled outlined rounded>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+                    </Row>
+
+                    <Row>
+                        <ButtonContainer>
+                            <Button color={BUTTON_COLOR.DESTRUCTIVE} contentColor={COLOR.WHITE} rounded>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button color={BUTTON_COLOR.DESTRUCTIVE} contentColor={COLOR.WHITE} hoverBackgroundColor={COLOR.BRIGHT_RED} rounded>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+
+                        <ButtonContainer>
+                            <Button disabled rounded>
+                                {BUTTON_TEXT}
+                            </Button>
+                        </ButtonContainer>
+                    </Row>
+                </Section>
+
+                {/* Tag components */}
+                <Section>
+                    <Row>
+                        <TagContainer>
+                            <Tag color='#00DBCE' contentColor={COLOR.WHITE}>Hello World</Tag>
+                        </TagContainer>
+                        <TagContainer>
+                            <Tag type='DELETE' color={COLOR.BRIGHT_RED} contentColor={COLOR.WHITE} onDelete={e=>alert('delete')}>Delete World</Tag>
+                        </TagContainer>
+                    </Row>
+                </Section>
+
+                {/* Typography */}
+                <Section>
+                  <Row>
+                      <Title1>
+                          Header1
+                      </Title1>
+                  </Row>
+                  <Row>
+                      <Title2>
+                          Header2
+                      </Title2>
+                  </Row>
+                  <Row>
+                      <Title3>
+                          Header3
+                      </Title3>
+                  </Row>
+                  <Row>
+                      <Title4>
+                          Header4
+                      </Title4>
+                  </Row>
+              </Section>
+
+
+            </Container>
+
             <Section>
                 <Row>
-                    <ButtonContainer>
-                        <Button color={BUTTON_COLOR.PRIMARY} contentColor={COLOR.MIDNIGHT_PURPLE} >
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button color={BUTTON_COLOR.PRIMARY} contentColor={COLOR.MIDNIGHT_PURPLE} hoverContentColor={COLOR.TEAL} hoverBackgroundColor={BUTTON_COLOR.HOVER_PRIMARY} >
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button disabled >
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                </Row>
-
-                <Row>
-                    <ButtonContainer>
-                        <Button color={BUTTON_COLOR.SECONDARY} contentColor={COLOR.MIDNIGHT_PURPLE} >
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button color={BUTTON_COLOR.SECONDARY} contentColor={COLOR.MIDNIGHT_PURPLE} hoverBackgroundColor={COLOR.LIGHT_GRAY}>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button disabled >
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                </Row>
-
-                <Row>
-                    <ButtonContainer>
-                        <Button color={COLOR.TRANSPARENT} contentColor={COLOR.NW_TEAL} hoverContentColor={COLOR.TEAL} outlined>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button color={COLOR.TRANSPARENT} contentColor={COLOR.NW_TEAL} hoverContentColor={COLOR.TEAL} hoverBackgroundColor={BUTTON_COLOR.HOVER_PRIMARY} outlined>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button disabled outlined>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-                </Row>
-
-                <Row>
-                    <ButtonContainer>
-                        <Button color={BUTTON_COLOR.DESTRUCTIVE} contentColor={COLOR.WHITE}>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button color={BUTTON_COLOR.DESTRUCTIVE} contentColor={COLOR.WHITE} hoverBackgroundColor={COLOR.BRIGHT_RED} >
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button disabled>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-                </Row>
-
-            </Section>
-
-            <Section>
-                <Row>
-                    <ButtonContainer>
-                        <Button color={BUTTON_COLOR.PRIMARY} contentColor={COLOR.MIDNIGHT_PURPLE} rounded>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button color={BUTTON_COLOR.PRIMARY} contentColor={COLOR.MIDNIGHT_PURPLE} hoverContentColor={COLOR.TEAL} hoverBackgroundColor={BUTTON_COLOR.HOVER_PRIMARY} rounded>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button disabled rounded>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                </Row>
-
-                <Row>
-                    <ButtonContainer>
-                        <Button color={BUTTON_COLOR.SECONDARY} contentColor={COLOR.MIDNIGHT_PURPLE} rounded>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button color={BUTTON_COLOR.SECONDARY} contentColor={COLOR.MIDNIGHT_PURPLE} hoverBackgroundColor={COLOR.LIGHT_GRAY} rounded>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button disabled rounded>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                </Row>
-
-                <Row>
-                    <ButtonContainer>
-                        <Button color={COLOR.TRANSPARENT} contentColor={COLOR.NW_TEAL} hoverContentColor={COLOR.TEAL} outlined rounded>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button color={COLOR.TRANSPARENT} contentColor={COLOR.NW_TEAL} hoverContentColor={COLOR.TEAL} hoverBackgroundColor={BUTTON_COLOR.HOVER_PRIMARY} outlined rounded>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button disabled outlined rounded>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-                </Row>
-
-                <Row>
-                    <ButtonContainer>
-                        <Button color={BUTTON_COLOR.DESTRUCTIVE} contentColor={COLOR.WHITE} rounded>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button color={BUTTON_COLOR.DESTRUCTIVE} contentColor={COLOR.WHITE} hoverBackgroundColor={COLOR.BRIGHT_RED} rounded>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
-
-                    <ButtonContainer>
-                        <Button disabled rounded>
-                            {BUTTON_TEXT}
-                        </Button>
-                    </ButtonContainer>
+                    <ScoreInput
+                        label="Sample Label"
+                        score={3}
+                        comment="Sample comment"
+                        handleClick={() => {}}
+                        handleTextChange={() => {}}
+                        maxScore={{value: 7, weight: 1}}/>
                 </Row>
             </Section>
-
-            {/* Tag components */}
-            <Section>
-                <Row>
-                    <TagContainer>
-                        <Tag color='#00DBCE' contentColor={COLOR.WHITE}>Hello World</Tag>
-                    </TagContainer>
-                    <TagContainer>
-                        <Tag type='DELETE' color={COLOR.BRIGHT_RED} contentColor={COLOR.WHITE} onDelete={e=>alert('delete')}>Delete World</Tag>
-                    </TagContainer>
-                </Row>
-            </Section>
-
-            {/* Typography */}
-            <Section>
-                <Row>
-                    <Title1>
-                        Header1
-                    </Title1>
-                </Row>
-                <Row>
-                    <Title2>
-                        Header2
-                    </Title2>
-                </Row>
-                <Row>
-                    <Title3>
-                        Header3
-                    </Title3>
-                </Row>
-                <Row>
-                    <Title4>
-                        Header4
-                    </Title4>
-                </Row>
-            </Section>
-        </Container>
+        </>
     )
 }

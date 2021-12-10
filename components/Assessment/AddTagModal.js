c323c918dbf58859052a653dac77a2e6cfad2c43
Your Name <new2282307660@gmail.com>
2021-12-09 19:55:46 -0800

    notes

--- components/Assessment/AddTagModal.js
+++ components/Assessment/AddTagModal.js
@@ -17,7 +17,9 @@ export default function ExportModal({ setShowing }) {
 //     only return top 5 match alphabetical order
 // 2. a create option if what's typed in does not have an exact match in existing tag
 // 3. need a field to hold associated tag for an applicant?
+// 4. delete a tag, add a trash can icon (ask design for a trash 
+//    can icon but use placeholder for now) that deletes a tag and remove it from all applicants who previously had 
 
 // [TODO] questions
-// 1. should the tags be hardcoded or shored in firebase
-// 2. where do the tags go in the schema
+// 1. should the tags be hardcoded or stored in firebase
+// 2. insert tag property

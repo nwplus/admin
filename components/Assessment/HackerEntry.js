60ad9b2e10fcebc8f8b91fa6b3df2471e558339f
Your Name <new2282307660@gmail.com>
2021-12-17 20:34:24 -0800

    looks very close to spec

--- components/Assessment/HackerEntry.js
+++ components/Assessment/HackerEntry.js
@@ -1,53 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ASSESSMENT_COLOR, MAX_SCORE, COLOR } from '../../constants';
-import Tag from '../tag';
-
-const styles = {
-  nameEmailContainer: {
-    flex: 3,
-    textAlign: 'left',
-  },
-  indexScoreContainer: {
-    flex: 1,
-    textAlign: 'right',
-  },
-  unselectedHackerContainer: {},
-  selectedHackerContainer: {},
-};
 
 const HackerName = styled.p`
   font-size: 16px;
-  color: ${ASSESSMENT_COLOR.DARK_GRAY};
+  color: ${COLOR.MIDNIGHT_PURPLE};
   margin: 0px;
   font-weight: bold;
 `;
 
-const LightGrayText = styled.p`
+const HackerInfoText = styled.p`
   font-size: 16px;
-  color: ${ASSESSMENT_COLOR.LIGHT_GRAY};
+  color: ${COLOR.MIDNIGHT_PURPLE};
   margin: 0px;
 `;
 
-const SelectedRowDiv = styled.div`
+const StyledHackerEntryDiv = styled.div`
   height: 56px;
   padding: 0 21px;
-  align-items: center;
   display: flex;
+  align-items: center;
+  gap: 18px;
   cursor: pointer;
   ${(p) => p.selected && `background: ${ASSESSMENT_COLOR.LIGHT_BLUE};`}
-`;
-
-const Scored = styled.p`
-  color: ${ASSESSMENT_COLOR.LIGHT_GRAY};
-  font-size: 16px;
-  margin: 0px;
-`;
-
-const Unscored = styled.p`
-  color: ${ASSESSMENT_COLOR.UNSCORED_GRAY};
-  font-size: 16px;
-  margin: 0px;
 `;
 
 const StyledTag = styled.div`
@@ -60,17 +35,17 @@ const StyledTag = styled.div`
   line-height: 16px;
   font-weight: 400;
   color: ${COLOR.LIGHT_PURPLE};
-  background: ${COLOR.MIDNIGHT_PURPLE_DARK};
+  background: ${COLOR.MIDNIGHT_PURPLE_MEDIUM};
   padding: 0px 5px;
   border-radius: 4px;
   height: 16px;
+  margin-left: 20px;
 `;
 
 export default function HackerEntry({
   index,
   firstName,
   lastName,
-  email,
   id,
   score,
   selectHacker,
@@ -78,28 +53,20 @@ export default function HackerEntry({
   selectedHackerID = null,
 }) {
   return (
-    <SelectedRowDiv
+    <StyledHackerEntryDiv
       onClick={() => selectHacker(id)}
       selected={id === selectedHackerID}
     >
       <div>{index}</div>
-      <div style={styles.nameEmailContainer}>
+      <div>
         <HackerName>
           {firstName} {lastName}
         </HackerName>
-        <LightGrayText>{email}</LightGrayText>
-      </div>
-      <div style={styles.indexScoreContainer}>
-        <LightGrayText>{id}</LightGrayText>
-        {score ? (
-          <Scored>
-            {score.totalScore ?? '?'}/{MAX_SCORE}
-          </Scored>
-        ) : (
-          <Unscored>/{MAX_SCORE}</Unscored>
-        )}
+        <HackerInfoText>
+          ID #{id} | Score: {score ? score.totalScore ?? '?' : '0'} /{MAX_SCORE}
+        </HackerInfoText>
       </div>
       {hasCompleted && <StyledTag>Completed</StyledTag>}
-    </SelectedRowDiv>
+    </StyledHackerEntryDiv>
   );
 }
--- constants.js
+++ constants.js
@@ -1,125 +1,125 @@
-export const EDIT = "EDIT";
-export const NEW = "NEW";
-export const VIEW = "VIEW";
-export const CLOSE = "CLOSE";
-export const DELETE = "DELETE";
-export const SAVE = "SAVE";
+export const EDIT = 'EDIT';
+export const NEW = 'NEW';
+export const VIEW = 'VIEW';
+export const CLOSE = 'CLOSE';
+export const DELETE = 'DELETE';
+export const SAVE = 'SAVE';
 export const COLOR = {
-  BACKGROUND: "#F8F8F8",
-  WHITE: "#FFFFFF",
-  GRAY: "#EDEDED",
-  PRIMARY: "#2D2937",
-  PRIMARY_DARK: "#1b1821",
-  TEXT: "#5A5A5A",
-  BLACK: "#000000",
-  DARK_COPY: "#A198A6",
-  RED: "#EB5757",
-  DARK_GRAY: "#606060",
-  TRANSPARENT: "Transparent",
-  BODY_TEXT: "#5A5A5A",
-  BLUE: "#4285F4",
-  UNSELECTED_GRAY: "#BDBAC3",
-  MIDNIGHT_PURPLE_DARK: "#433860",
-  MIDNIGHT_PURPLE: "#2C2543",
-  MIDNIGHT_PURPLE_LIGHT: "#8E7EB4",
-  LIGHT_GRAY: "#F0EEF2",
-  LIGHT_PURPLE: "#E2D6FF",
-  NW_TEAL: "#20FFAF",
-  TEAL: "#00A399",
-  INACTIVE_DARK_GRAY: "#8C898F",
-  BRIGHT_RED: "#F83D3D",
-  GREY_500: "#BDBAC3",
+  BACKGROUND: '#F8F8F8',
+  WHITE: '#FFFFFF',
+  GRAY: '#EDEDED',
+  PRIMARY: '#2D2937',
+  PRIMARY_DARK: '#1b1821',
+  TEXT: '#5A5A5A',
+  BLACK: '#000000',
+  DARK_COPY: '#A198A6',
+  RED: '#EB5757',
+  DARK_GRAY: '#606060',
+  TRANSPARENT: 'Transparent',
+  BODY_TEXT: '#5A5A5A',
+  BLUE: '#4285F4',
+  UNSELECTED_GRAY: '#BDBAC3',
+  MIDNIGHT_PURPLE: '#2C2543',
+  MIDNIGHT_PURPLE_MEDIUM: '#433860',
+  MIDNIGHT_PURPLE_LIGHT: '#8E7EB4',
+  LIGHT_GRAY: '#F0EEF2',
+  LIGHT_PURPLE: '#E2D6FF',
+  NW_TEAL: '#20FFAF',
+  TEAL: '#00A399',
+  INACTIVE_DARK_GRAY: '#8C898F',
+  BRIGHT_RED: '#F83D3D',
+  GREY_500: '#BDBAC3',
 };
 export const BUTTON_COLOR = {
-  PRIMARY: "linear-gradient(92.58deg, #0DEFE1 0%, #78FF96 100%)",
-  SECONDARY: "#FFFFFF",
-  OUTLINE: "Transparent",
-  DESTRUCTIVE: "#F65C5C",
-  HOVER_PRIMARY: "linear-gradient(90deg, #D7FFF0 0%, #7BFFCF 100%)",
+  PRIMARY: 'linear-gradient(92.58deg, #0DEFE1 0%, #78FF96 100%)',
+  SECONDARY: '#FFFFFF',
+  OUTLINE: 'Transparent',
+  DESTRUCTIVE: '#F65C5C',
+  HOVER_PRIMARY: 'linear-gradient(90deg, #D7FFF0 0%, #7BFFCF 100%)',
 };
-export const FAQ = "FAQ";
+export const FAQ = 'FAQ';
 export const FAQCategory = Object.freeze({
-  GENERAL: "General",
-  LOGS: "Logistics",
-  TEAMS: "Teams & Projects",
-  MISC: "Miscellaneous",
+  GENERAL: 'General',
+  LOGS: 'Logistics',
+  TEAMS: 'Teams & Projects',
+  MISC: 'Miscellaneous',
 });
-export const SPONSORSHIP = "SPONSORSHIP";
+export const SPONSORSHIP = 'SPONSORSHIP';
 export const HACKATHON_NAVBAR = {
-  intro: "Intro",
-  events: "Events",
-  spocos: "Sponsors",
-  FeatureFlags: "Feature Flags",
-  BuildConfig: "Build Config",
+  intro: 'Intro',
+  events: 'Events',
+  spocos: 'Sponsors',
+  FeatureFlags: 'Feature Flags',
+  BuildConfig: 'Build Config',
 };
 export const LIVESITE_NAVBAR = {
-  announcements: "Announcements",
-  quicklinks: "Quicklinks",
-  schedule: "Schedule",
-  settings: "Settings",
-  judging: "Judging",
+  announcements: 'Announcements',
+  quicklinks: 'Quicklinks',
+  schedule: 'Schedule',
+  settings: 'Settings',
+  judging: 'Judging',
 };
 
 export const ASSESSMENT_COLOR = {
-  PRIMARY: "#2D2937",
-  PRIMARY_DARK: "#1b1821",
-  TEXT: "#5A5A5A",
-  BLACK: "#000000",
-  DARK_COPY: "#A198A6",
-  RED: "#EB5757",
-  DARK_GRAY: "#4F4F4F",
-  TRANSPARENT: "Transparent",
-  BODY_TEXT: "#5A5A5A",
-  LIGHT_GRAY: "#828282",
-  UNSCORED_GRAY: "#E0E0E0",
-  LIGHT_BLUE: "#F0EEF2",
-  BLUE_TEXT: "#2F80ED",
-  TOOLBAR_GRAY: "#FAFAFA",
-  BLUE_BORDER: "#21258A",
+  PRIMARY: '#2D2937',
+  PRIMARY_DARK: '#1b1821',
+  TEXT: '#5A5A5A',
+  BLACK: '#000000',
+  DARK_COPY: '#A198A6',
+  RED: '#EB5757',
+  DARK_GRAY: '#4F4F4F',
+  TRANSPARENT: 'Transparent',
+  BODY_TEXT: '#5A5A5A',
+  LIGHT_GRAY: '#828282',
+  UNSCORED_GRAY: '#E0E0E0',
+  LIGHT_BLUE: '#F0EEF2',
+  BLUE_TEXT: '#2F80ED',
+  TOOLBAR_GRAY: '#FAFAFA',
+  BLUE_BORDER: '#21258A',
 };
 
 // Assessment portal
 export const APPLICATION_STATUS = {
   applied: {
     color: ASSESSMENT_COLOR.RED,
-    textColor: "white",
-    text: "applied",
+    textColor: 'white',
+    text: 'applied',
   },
   scored: {
     color: ASSESSMENT_COLOR.BLUE_TEXT,
-    textColor: "white",
-    text: "scored",
+    textColor: 'white',
+    text: 'scored',
   },
   accepted: {
-    color: "green",
-    textColor: "white",
-    text: "accepted",
+    color: 'green',
+    textColor: 'white',
+    text: 'accepted',
   },
   completed: {
     color: COLOR.MIDNIGHT_PURPLE,
-    textColor: "white",
-    text: "completed",
+    textColor: 'white',
+    text: 'completed',
   },
 };
 
 export const SCORING = {
   LINK: {
-    label: "GitHub/Personal Website",
+    label: 'GitHub/Personal Website',
     value: 7,
     weight: 1,
   },
   RESUME: {
-    label: "Resume",
+    label: 'Resume',
     value: 7,
     weight: 1,
   },
   ESSAY: {
-    label: "Short Answer 1",
+    label: 'Short Answer 1',
     value: 3,
     weight: 2,
   },
   ESSAY_TWO: {
-    label: "Short Answer 2",
+    label: 'Short Answer 2',
     value: 3,
     weight: 2,
   },
@@ -131,15 +131,15 @@ export const MAX_SCORE = Object.values(SCORING).reduce(
 );
 
 export const SORT = {
-  TIMESTAMP: "Timestamp",
-  LAST_NAME: "Last Name",
-  FIRST_NAME: "First Name",
-  SCORE: "Total Score",
-  STATUS: "Status",
+  TIMESTAMP: 'Timestamp',
+  LAST_NAME: 'Last Name',
+  FIRST_NAME: 'First Name',
+  SCORE: 'Total Score',
+  STATUS: 'Status',
 };
 
 export const TABS = {
-  OVERVIEW: "Overview",
-  RESUME: "Resume",
-  COMMENTS: "Comments",
+  OVERVIEW: 'Overview',
+  RESUME: 'Resume',
+  COMMENTS: 'Comments',
 };

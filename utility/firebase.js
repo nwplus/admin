be5bf935974176bed861e85707b06f4a327982e4
Jae Wu <jaewuchun@gmail.com>
2022-12-05 20:13:12 -0800

    switched eval hackathon to nwHacks 2023

--- utility/firebase.js
+++ utility/firebase.js
@@ -32,7 +32,7 @@ const Hackathons = 'Hackathons';
 const InternalWebsitesCollection = 'InternalWebsites';
 const CMSCollection = 'CMS';
 const LivesiteCollection = 'Livesite';
-const HackerEvaluationHackathon = 'cmd-f2022';
+const HackerEvaluationHackathon = 'nwHacks2023';
 
 export const getTimestamp = () => {
   return firebase.firestore.Timestamp.now();

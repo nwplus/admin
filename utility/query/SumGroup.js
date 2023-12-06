b388789c1c4dbdf567b625ea8cf57aa8b9605d1a
alvinkam <alvin.kam.33@gmail.com>
2023-12-05 23:23:34 -0800

    querying functionality

---
+++ utility/query/SumGroup.js
@@ -0,0 +1,18 @@
+export default class SumGroup {
+  constructor(applyKey, key) {
+    this.applyKey = applyKey;
+    this.key = key;
+  }
+
+  getApplyKey() {
+    return this.applyKey;
+  }
+
+  executeApplyRuleOnGroup(groupEntries) {
+    let sum = 0;
+    groupEntries.forEach((entry) => {
+      sum += Number(entry[this.key]);
+    });
+    return Number(sum.toFixed(2));
+  }
+}

a63b4ea2811d0ab4c0b7de46160c772b08405c0d
jasonkuoubc <jason.kuo@ubc.ca>
2024-12-08 21:12:33 -0800

    hello

--- utility/normalization.js
+++ utility/normalization.js
@@ -1,17 +1,55 @@
 import { getAllGradedApplicants } from './firebase'
 
+// this is what the data looks like in firebase
+// {
+// 	[applicantId: string]: {
+// 		score: {
+// 			// ... other stuff
+// 			scores: {
+// 				[questionName: string]: {
+// 					lastUpdated: Date
+// 					lastUpdatedBy: string
+// 					score: number
+// 				}
+// 			}
+// 		}
+// 	}
+// }
+
+// this is what the data looks like after transformation
+// {
+// 	"Jason": {
+// 		"Q1": [[score, appId], [score2, appId2]]
+// 	}
+// }
+
+
 export const transformScores = () => {
+  const result = {}
+
   getAllGradedApplicants(applicants => {
     applicants.forEach(applicant => {
       const {
         _id,
         score: { scores },
       } = applicant
-      console.log('id', _id)
-      Object.entries(scores).forEach(([key, value]) => {
+
+      Object.entries(scores).forEach(([questionName, value]) => {
         const { lastUpdatedBy, score } = value
-        console.log(key, lastUpdatedBy, score)
+        
+        if (!result[lastUpdatedBy]) {
+          result[lastUpdatedBy] = {}
+        }
+        
+        if (!result[lastUpdatedBy][questionName]) {
+          result[lastUpdatedBy][questionName] = []
+        }
+        
+        result[lastUpdatedBy][questionName].push([score, _id])
       })
     })
+
+    console.log('Transformed data:', result)
+    return result
   })
 }

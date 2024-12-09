49de4922b8a8457e3c602418de551370505b93bf
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-12-08 21:29:01 -0800

    Turn transformScores into promise

--- utility/normalization.js
+++ utility/normalization.js
@@ -24,55 +24,54 @@ import * as math from 'mathjs'
 // 	}
 // }
 
-
 export const transformScores = () => {
   const result = {}
 
-  getAllGradedApplicants(applicants => {
-    applicants.forEach(applicant => {
-      const {
-        _id,
-        score: { scores },
-      } = applicant
+  return new Promise(resolve => {
+    getAllGradedApplicants(applicants => {
+      applicants.forEach(applicant => {
+        const {
+          _id,
+          score: { scores },
+        } = applicant
 
-      Object.entries(scores).forEach(([questionName, value]) => {
-        const { lastUpdatedBy, score } = value
-        
-        if (!result[lastUpdatedBy]) {
-          result[lastUpdatedBy] = {}
-        }
-        
-        if (!result[lastUpdatedBy][questionName]) {
-          result[lastUpdatedBy][questionName] = []
-        }
-        
-        result[lastUpdatedBy][questionName].push([score, _id])
+        Object.entries(scores).forEach(([questionName, value]) => {
+          const { lastUpdatedBy, score } = value
+
+          if (!result[lastUpdatedBy]) {
+            result[lastUpdatedBy] = {}
+          }
+
+          if (!result[lastUpdatedBy][questionName]) {
+            result[lastUpdatedBy][questionName] = []
+          }
+
+          result[lastUpdatedBy][questionName].push([score, _id])
+        })
       })
-    })
 
-    console.log('Transformed data:', result)
-    return result
+      console.log('Transformed data:', result)
+      resolve(result)
+    })
   })
 }
 
-export const calculateNormalizedScores = () => {
-    const data = transformScores();
-    console.log('Test data:', data);
-    const normalizedScores = {};
-    for (const grader in data) {
-        normalizedScores[grader] = {};
-        for (const question in data[grader]) {
-            const scores = data[grader][question].map(([score, appId]) => score);
-            const mean = math.mean(scores);
-            const stdDev = math.std(scores);
-            normalizedScores[grader][question] = data[grader][question].map(([score, appId]) => [
-                stdDev === 0 ? 0 : (score - mean) / stdDev,
-                appId
-            ]);
-        }
+export const calculateNormalizedScores = async () => {
+  const data = await transformScores()
+  console.log('Test data:', data)
+  const normalizedScores = {}
+  for (const grader in data) {
+    normalizedScores[grader] = {}
+    for (const question in data[grader]) {
+      const scores = data[grader][question].map(([score, appId]) => score)
+      const mean = math.mean(scores)
+      const stdDev = math.std(scores)
+      normalizedScores[grader][question] = data[grader][question].map(([score, appId]) => [
+        stdDev === 0 ? 0 : (score - mean) / stdDev,
+        appId,
+      ])
     }
-    console.log(normalizedScores);
-    return normalizedScores;
+  }
+  console.log(normalizedScores)
+  return normalizedScores
 }
-
-
